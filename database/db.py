4f618ccd5d7e04814098bedcd8b09d52b62e2b5e
Michaelgathara <mikegtrm@gmail.com>
2025-01-10 04:08:00 -0600

    db: added browser db

--- database/db.py
+++ database/db.py
@@ -2,7 +2,7 @@ from typing import List
 import sqlite3
 
 class ResearchDatabase:
-    def __init__(self, db_path, str = 'research_data.db'):
+    def __init__(self, db_path: str = 'research_data.db'):
         self.conn = sqlite3.connect(db_path)
         self.create_table()
 
@@ -21,6 +21,37 @@ class ResearchDatabase:
             self.conn.executemany(
                 'INSERT INTO research (topic, summary) VALUES (?, ?)',
                 zip(topics, summaries)
+            )
+    
+    def close(self):
+        self.conn.close()
+        
+class BrowserUseDatabase:
+    def __init__(self, db_path: str = 'browser_use_data.db') -> None:
+        self.conn = sqlite3.connect(db_path)
+        self.create_table()
+    
+    def create_table(self):
+        with self.conn:
+            self.conn.execute('''
+                              CREATE TABLE IF NOT EXISTS browser(
+                                  id INTEGER PRIMARY KEY,
+                                  visited_urls TEXT,
+                                  screenshot_paths TEXT,
+                                  action_names TEXT,
+                                  errors TEXT,
+                                  model_actions TEXT
+                              )
+                              ''')
+    
+    def save_results(self, visited_urls: List[str], screenshot_paths: List[str], action_names: List[str], errors: list, model_actions: List[dict]):
+        # https://github.com/browser-use/browser-use/blob/main/browser_use/agent/views.py#L111 for the types.
+        # TODO: check the types for errors
+        
+        with self.conn:
+            self.conn.executemany(
+                'INSERT INTO browser (visited_urls, screenshot_paths, action_names, errors, model_actions) VALUES (?, ?, ?, ?, ?)',
+                zip(visited_urls, screenshot_paths, action_names, errors, model_actions)
             )
     
     def close(self):
